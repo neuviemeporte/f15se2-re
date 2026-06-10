86e408a9298f23181dd679dbba80295f7e8ceaf1
Anders Jenbo <anders@jenbo.dk>
2026-06-10 11:43:19 +0200

    Fix drawProjectionSphere mapping

--- src/egfileio.c
+++ src/egfileio.c
@@ -26,19 +26,20 @@ void drawProjectionSphere(int arg_0)
     int e[17];
     int f[8];
     int g;
-    register int i;
-    register int j;
 
     if (*(char *)&word_38FDC < 3) {
         sub_1FEEC(arg_0);
         return;
     }
-    a = 0;
-    do {
-        i = a + a;
-        *((int *)((char *)&word_3BE9C + i)) = *((int *)((char *)&word_32990 + i));
-        a++;
-    } while (a < 16);
+    {
+        register int i;
+        a = 0;
+        do {
+            i = a + a;
+            *((int *)((char *)&word_3BE9C + i)) = *((int *)((char *)&word_32990 + i));
+            a++;
+        } while (a < 16);
+    }
     word_38FC6 = -var_226;
     p = (int)(((long)var_224 << 8) / (long)(var_225 < 0x200 ? 0x200 : var_225));
     if (var_594 != 0) {
@@ -47,20 +48,24 @@ void drawProjectionSphere(int arg_0)
     if (var_456 != 0) {
         p >>= 1;
     }
-    for (a = 0; a < 17; a++) {
-        if (a < 16) {
-            g = (&word_3BE9C)[a] + p;
-        } else {
-            g = 0x5848;
+    {
+        register int i;
+        register int j;
+        for (a = 0; a < 17; a++) {
+            if (a < 16) {
+                g = (&word_3BE9C)[a] + p;
+            } else {
+                g = 0x5848;
+            }
+            i = fixedMulQ14(-0x5848, var_227);
+            j = fixedMulQ14(g, word_38FC6);
+            b[a] = (word_3298C + i) - j;
+            d[a] = -i + word_3298C - j;
+            i = fixedMulQ14(g, var_227);
+            j = fixedMulQ14(-0x5848, word_38FC6);
+            c[a] = -(-((i + j >> 2) - i) + j) + word_3298E;
+            e[a] = ((i - j >> 2) + word_3298E) - i + j;
         }
-        i = fixedMulQ14(-0x5848, var_227);
-        j = fixedMulQ14(g, word_38FC6);
-        b[a] = (word_3298C + i) - j;
-        d[a] = -i + word_3298C - j;
-        i = fixedMulQ14(g, var_227);
-        j = fixedMulQ14(-0x5848, word_38FC6);
-        c[a] = -(-((i + j >> 2) - i) + j) + word_3298E;
-        e[a] = ((i - j >> 2) + word_3298E) - i + j;
     }
     a = 0;
     do {
@@ -82,20 +87,24 @@ void drawProjectionSphere(int arg_0)
         a++;
     } while (a < 16);
     word_3BE9C = 0;
-    for (a = 0; a < 17; a++) {
-        if (a < 16) {
-            g = p - (&word_3BE9C)[a];
-        } else {
-            g = -0x5848;
+    {
+        register int i;
+        register int j;
+        for (a = 0; a < 17; a++) {
+            if (a < 16) {
+                g = p - (&word_3BE9C)[a];
+            } else {
+                g = -0x5848;
+            }
+            i = fixedMulQ14(-0x5848, var_227);
+            j = fixedMulQ14(g, word_38FC6);
+            b[a] = (word_3298C + i) - j;
+            d[a] = -i + word_3298C - j;
+            i = fixedMulQ14(g, var_227);
+            j = fixedMulQ14(-0x5848, word_38FC6);
+            c[a] = -(-((i + j >> 2) - i) + j) + word_3298E;
+            e[a] = ((i - j >> 2) + word_3298E) - i + j;
         }
-        i = fixedMulQ14(-0x5848, var_227);
-        j = fixedMulQ14(g, word_38FC6);
-        b[a] = (word_3298C + i) - j;
-        d[a] = -i + word_3298C - j;
-        i = fixedMulQ14(g, var_227);
-        j = fixedMulQ14(-0x5848, word_38FC6);
-        c[a] = -(-((i + j >> 2) - i) + j) + word_3298E;
-        e[a] = ((i - j >> 2) + word_3298E) - i + j;
     }
     a = 0;
     do {
